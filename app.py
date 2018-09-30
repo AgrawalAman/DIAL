d27275e6d58da07386c8a333c0153ff9e4f60b11
AgrawalAman <aa3539@drexel.edu>
2018-09-30 06:49:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os, redis
 account_sid = 'AC789b798bde070df6992f4f0fba84e89a'
 auth_token = '54c50792327d057f2847007947f11cc3'
 client = Client(account_sid, auth_token)
-db=redis.from_url(os.environ['REDISCLOUD_URL'])
+db=redis.from_url(os.environ['REDIS_URL'])
 
 app = Flask(__name__)
         
