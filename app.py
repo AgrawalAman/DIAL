68e07e0a3839647275e8a492f634e2a54620bf4c
AgrawalAman <aa3539@drexel.edu>
2018-09-30 07:56:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def handleMsg(userNumber, queueNumber, text):
         relayMsg(userNumber, text, queueNumber)
         return
    
-    elif queues[queueNumber] is not None:
+    elif queues is None:
         #todo bug probably
         addUserToQueue(userNumber, queueNumber)
         sendMsg(userNumber, "Please wait to be paired with another user. Reply STOP to opt out of the service.", queueNumber)
