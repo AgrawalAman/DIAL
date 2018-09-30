b94a7970a68e9b7c2e2fc687177668f83fce4bf4
davidkim827 <davidkim827@gmail.com>
2018-09-29 20:50:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+from twilio.rest import Client
 from flask import Flask
 from datetime import datetime
+
+account_sid = 'AC789b798bde070df6992f4f0fba84e89a'
+auth_token = '54c50792327d057f2847007947f11cc3'
+client = Client(account_sid, auth_token)
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,6 +19,16 @@ def homepage():
     <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
 
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming messages with a friendly SMS."""
+    # Start our response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("This is a reply.")
+
+    return str(resp)
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
-
